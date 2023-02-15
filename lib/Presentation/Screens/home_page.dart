d3ed493790ecb919f0a22c88e2a8fc43660f013e
vijayinyoutube <vijaythevirtue@gmail.com>
2023-02-15 08:35:07 +0530

    first commit

---
+++ lib/Presentation/Screens/home_page.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  final String imageUrl =
+      'https://user-images.githubusercontent.com/58719230/218909229-67867fec-6f4a-43fb-bfc3-33d6bc42ae2e.png';
+
+  @override
+  void initState() {
+    _controller =
+        AnimationController(vsync: this, duration: const Duration(seconds: 20))
+          ..repeat();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: NetworkImage(imageUrl), fit: BoxFit.fitHeight),
+          ),
+          child: RadarSignal(controller: _controller)),
+    );
+  }
+}
+
+class RadarSignal extends StatelessWidget {
+  const RadarSignal({
+    super.key,
+    required AnimationController controller,
+  }) : _controller = controller;
+
+  final AnimationController _controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return RotationTransition(
+      turns: Tween(begin: 0.0, end: 4.0).animate(_controller),
+      child: Container(
+        decoration: const BoxDecoration(
+          gradient: SweepGradient(
+            center: FractionalOffset.center,
+            colors: <Color>[
+              Colors.transparent,
+              Color(0xFF34A853),
+              Colors.transparent,
+            ],
+            stops: <double>[0.20, 0.25, 0.20],
+          ),
+        ),
+      ),
+    );
+  }
+}
